0e77df1fd9b831d14cad774be7ae0858532b8186
Shivam Singh <shivamsingh@Shivams-Mac.local>
2025-11-11 13:07:37 +0530

    resumeChange

--- components/intro.tsx
+++ components/intro.tsx
@@ -81,7 +81,7 @@ I enjoy building{" "}
           setTimeofLastClick(Date.now())
         }}
         >Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/></Link>
-        <a href='/mern.pdf' download target="_blank" className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition-all borderBlack dark:bg-white/10'>
+        <a href='/resume1.pdf' download target="_blank" className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition-all borderBlack dark:bg-white/10'>
           Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
         <a href="https://www.linkedin.com/in/singh-shvam/" className= 'bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all borderBlack dark:bg-white/10 dark:text-white/60' target='_blank'>
